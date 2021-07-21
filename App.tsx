63d2f34d0a79495eea4ad1dc43ae8f63b06234b0
mohamedsalem20 <reda.technosignage@gmail.com>
2021-07-21 16:44:24 +0100

    init project

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import CharacterList from './app/Screens/characterList/characterList';
+
+const App = () => {
+  return <CharacterList />;
+};
+
+export default App;
---
+++ app/Screens/characterList/characterList.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+const CharacterList = () => {
+  return <Text>CharacterList Screen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default CharacterList;
